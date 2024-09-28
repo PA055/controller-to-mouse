25da6cfc2b18d659001c2057a4ad8cf1da71adf2
lufimio <67525443+PA055@users.noreply.github.com>
2024-09-28 15:08:20 -0400

    feat: :construction: config json

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ for _ in range(50):
     time.sleep(0.1)
 if not any(XInput.get_connected()): 
     print("Failed to connect, try again later")
-    # exit(1)
+    exit(1)
 controller_index = XInput.get_connected().index(True)
 
 last = time.time()
@@ -20,6 +20,6 @@ while True:
     buttons = XInput.get_button_values(state)
     (LX, LY), _ = XInput.get_thumb_values(state)
     pyautogui.move(LX * 50, LY * -50)
-    print(dt)
+
     dt = time.time() - last
     last = time.time()
