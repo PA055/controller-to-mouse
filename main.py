e0d8842e298d92201e6e11b620bf587c8bb83594
lufimio <67525443+PA055@users.noreply.github.com>
2024-09-28 21:52:03 -0400

    feat: :construction: keyboard snapping

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import XInput
 import mouse
 import keyboard
 import time
+import win32gui
  
 sensitivity = 20
-key_height = 0.165
 keyboard_map = [
-    [0.051] * 13 + [0.096, 0.061, 0.065, 0.061],
-    [0.077] + [0.051] * 13 + [0.070, 0.061, 0.065, 0.061],
-    [0.103] + [0.051] * 11 + [0.146, 0.061, 0.065, 0.061],
-    [0.129] + [0.051] * 11 + [0.120, 0.061, 0.065, 0.061],
-    [0.051] * 3 + [0.284] + [0.051] * 5 + [0.067, 0.061, 0.065, 0.061],
+    ([.03866, .107388, .168385, .235395, .297251, .358247, .421821, .481959, .548969, .610825, .67268, .74055, .802405, .867698, .945876], .2292042), 
+    ([.056701, .135739, .20189, .261168, .325601, .390893, .454467, .518041, .580756, .645189, .707904, .771478, .835052, .898625, .962199], .3705943), 
+    ([.06701, .165808, .231959, .295533, .359107, .421821, .486254, .548969, .61512, .677835, .74055, .804124, .908935], .511736), 
+    ([.080756, .199313, .265464, .325601, .388316, .454467, .518041, .582474, .643471, .710481, .773196, .834192, .931271], .662329), 
+    ([.037801, .103952, .165808, .232818, .44244, .643471, .705326, .769759, .832474, .898625, .962199], .811865)
 ]
 
 def get_joysticks() -> tuple[tuple[float, float], tuple[float, float]]:
@@ -48,12 +48,13 @@ def get_triggers() -> tuple[float, float]:
     if controller_index >= 0: return XInput.get_trigger_values(XInput.get_state(controller_index))
     return 1 if keyboard.is_pressed('x') else 0, 1 if keyboard.is_pressed('c') else 0
 
+import contextlib
 controller_index = -1
 print("Checking for connected controllers...")
 
 for _ in range(50):
     if any(XInput.get_connected()): break
-    time.sleep(0.1)
+    time.sleep(.1)
 if not any(XInput.get_connected()): 
     print("Failed to connect, try again later")
     # exit(1)
@@ -63,44 +64,55 @@ else:
 
 last = time.time()
 last_buttons = {key: False for key in get_buttons()}
-last_triggers = (0.0, 0.0)
-dt = 0.0
+last_triggers = (.0, .0)
+dt = .0
 mode = 'enabled'
-keyboard_open = False
+
+keyboard_open_time = 0.5
+keyboard_win = None
+keyboard_win_rect = (.0, .0, .0, .0)
+keyboard_index = (0, 0)
+
 while True:
     buttons = get_buttons()
     LT, RT = get_triggers()
     (LX, LY), (RX, RY) = get_joysticks()
     if mode == 'enabled':
-        mouse.move(LX * sensitivity, LY * -sensitivity, absolute=False, duration=0.01)
+        mouse.move(LX * sensitivity, LY * -sensitivity, absolute=False, duration=.01)
         if RY != 0: mouse.wheel(RY)
         if RX != 0:
             keyboard.press("SHIFT")
             mouse.wheel(-RX)
             keyboard.release("SHIFT")
-        
-    if mode == 'keyboard' and not keyboard_open:
-        pass
 
+    if mode == 'keyboard' and keyboard_open_time > 0:
+        keyboard_open_time -= dt
+        if keyboard_open_time < 0:
+            with contextlib.suppress(Exception):
+                keyboard_win = win32gui.FindWindow(None, "On-Screen Keyboard")
+                keyboard_win_rect = win32gui.GetWindowRect(keyboard_win)
+        
+    if mode == 'keyboard' and keyboard_open_time < 0:
+        width, height  = keyboard_win_rect[2] - keyboard_win_rect[0], keyboard_win_rect[3] - keyboard_win_rect[1]
+        key_x = keyboard_win_rect[0] + (keyboard_map[keyboard_index[1]][0][keyboard_index[0]] * (width - 214))
+        key_y = keyboard_win_rect[1] + (keyboard_map[keyboard_index[1]][1] * (height)) + 30
+        mouse.move(key_x, key_y)
+         
     if mode != 'disabled':
         if (not (last_buttons["DPAD_DOWN"] and last_buttons["LEFT_SHOULDER"])) and buttons["DPAD_DOWN"] and buttons["LEFT_SHOULDER"]:
             keyboard.send('ctrl+win+o')
-            keyboard_open = False
+            keyboard_open_time = 0.5
             mode = 'enabled' if mode == 'keyboard' else 'keyboard'
-        
-        if LT > .3 and last_triggers[0] < .3: 
-            mouse.press('right')
-        if LT < .3 and last_triggers[0] > .3:
-            mouse.release('right')
-            
-        if RT > .3 and last_triggers[1] < .3:
-            mouse.press('left')
-        if RT < .3 and last_triggers[1] > .3:
-            mouse.release('left')
-        
+
+        if LT >= .3 and last_triggers[0] < .3: mouse.press('right')
+        if LT <= .3 and last_triggers[0] > .3: mouse.release('right')
+
+        if RT >= .3 and last_triggers[1] < .3: mouse.press('left')
+        if RT <= .3 and last_triggers[1] > .3: mouse.release('left')
+
     if buttons["START"] and buttons["LEFT_SHOULDER"] and not (last_buttons["START"] and last_buttons["LEFT_SHOULDER"]):
         mode = 'enabled' if mode == 'disabled' else 'disabled'
-    
+
     dt = time.time() - last
     last = time.time()
     last_buttons = buttons
--- test2.py
+++ test2.py
@@ -1,30 +1,37 @@
-import keyboard
+import win32gui
 import mouse
+import keyboard
 
-resolution=[1920,1080]
-xpos=0
-ypos=0
-sensitivity=2
-xposprev=0
-yposprev=0   
+window = win32gui.FindWindow(None, "On-Screen Keyboard")
+left, top, right, bottom = win32gui.GetWindowRect(window)
+width, height  = right - left, bottom - top
 
+keyboard_map = []
+y_avg = 0.0
+x_pos: list[float] = []
 while True:
-    ypos=0
-    xpos=0
-    if keyboard.is_pressed("up"):
-        ypos=-1.0
-    if keyboard.is_pressed("down"):
-        ypos=1.0
-    if keyboard.is_pressed("left"):
-        xpos=-1.0
-    if keyboard.is_pressed("right"):
-        xpos=1.0
-    ypos*=sensitivity
-    xpos*=sensitivity
-     
-    mouse.move(xpos, ypos, absolute=False, duration=0.001)
-    
-    
-    
-    
-    print(str(xpos) + "   " + str(ypos))
+    keyboard.read_key()
+    if (keyboard.is_pressed('enter')):
+        keyboard_map.append((x_pos[:], y_avg))
+        y_avg = 0
+        x_pos = []
+        print(f"line: {y_avg}\n")
+    if keyboard.is_pressed('space'):
+        x, y = round((mouse.get_position()[0] - left) / (width - 214), 6), round((mouse.get_position()[1] - top - 30) / height, 6)
+        y_avg = (y_avg * len(x_pos) + y) / (len(x_pos) + 1)
+        x_pos.append(x)
+        print(x, y)
+    if keyboard.is_pressed('escape'):
+        keyboard_map.append((x_pos[:], y_avg))
+        break
+print(keyboard_map)
+
+
+keyboard_map = [
+    ([0.03866, 0.107388, 0.168385, 0.235395, 0.297251, 0.358247, 0.421821, 0.481959, 0.548969, 0.610825, 0.67268, 0.74055, 0.802405, 0.867698, 0.945876], 0.22920420000000005), 
+    ([0.056701, 0.135739, 0.20189, 0.261168, 0.325601, 0.390893, 0.454467, 0.518041, 0.580756, 0.645189, 0.707904, 0.771478, 0.835052, 0.898625, 0.962199], 0.37059426666666667), 
+    ([0.06701, 0.165808, 0.231959, 0.295533, 0.359107, 0.421821, 0.486254, 0.548969, 0.61512, 0.677835, 0.74055, 0.804124, 0.908935], 0.5117359999999999), 
+    ([0.080756, 0.199313, 0.265464, 0.325601, 0.388316, 0.454467, 0.518041, 0.582474, 0.643471, 0.710481, 0.773196, 0.834192, 0.931271], 0.6623286153846155), 
+    ([0.037801, 0.103952, 0.165808, 0.232818, 0.44244, 0.643471, 0.705326, 0.769759, 0.832474, 0.898625, 0.962199], 0.8118648181818181)
+]
+
