1cfe332dc1838e8121c4120893a692db15ecbf1b
lufimio <67525443+PA055@users.noreply.github.com>
2024-09-29 02:47:52 -0400

    feat: :sparkles: a better keyboard ??
    the keyboard can now be entered from any direction and it snaps to the right key, going up and down is mostly fixed but left and right is still a bit unreliable

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from typing import Any
 import XInput
 import mouse
 import keyboard
 import time
+import math
 import win32gui
 import contextlib
 import button
@@ -12,8 +14,8 @@ dt = .0
 mode = 'enabled'
 
 keyboard_open_time = 0.5
-keyboard_win_rect = (.0, .0, .0, .0)
-keyboard_index = [0, 0]
+keyboard_win: Any = None
+mouse_pos = [0, 0]
 
 
 sensitivity = 20
@@ -25,100 +27,120 @@ keyboard_map = [
     ([.037801, .103952, .165808, .232818, .44244, .643471, .705326, .769759, .832474, .898625, .962199], .811865)
 ]
 
+def index_to_window(index: tuple[int, int]) -> tuple[float, float]:
+    global mode
+    try:
+        keyboard_win_rect = win32gui.GetWindowRect(keyboard_win)
+    except Exception:
+        mode = 'enabled'
+        return (0, 0)
+    width, height = keyboard_win_rect[2] - keyboard_win_rect[0], keyboard_win_rect[3] - keyboard_win_rect[1]
+    key_x = keyboard_win_rect[0] + (keyboard_map[index[1]][0][index[0]] * (width - 214))
+    key_y = keyboard_win_rect[1] + (keyboard_map[index[1]][1] * (height)) + 30
+    return key_x, key_y
+
+def snap_keyboard(pos: tuple[int, int]) -> tuple[int, int]:
+    global mode
+    try:
+        keyboard_win_rect = win32gui.GetWindowRect(keyboard_win)
+    except Exception:
+        mode = 'enabled'
+        return (0, 0)
+    width, height = keyboard_win_rect[2] - keyboard_win_rect[0], keyboard_win_rect[3] - keyboard_win_rect[1]
+    x_index, y_index = -1, -1
+    dist = math.inf
+    for i, (row, y) in enumerate(keyboard_map):
+        for j, x in enumerate(row):
+            key_x = keyboard_win_rect[0] + (x * (width - 214))
+            key_y = keyboard_win_rect[1] + (y * (height)) + 30
+            if math.hypot(key_x - pos[0], key_y - pos[1]) < dist:
+                x_index, y_index = j, i
+                dist = math.hypot(key_x - pos[0], key_y - pos[1])
+    return x_index, y_index
     
 def keyboard_controls():
-    global keyboard_index
-    keyboard_win_rect = win32gui.GetWindowRect(keyboard_win)
-    if keyboard_index[1] >= 0 and keyboard_index[1] < len(keyboard_map) and \
-        keyboard_index[0] >= 0 and keyboard_index[0] < len(keyboard_map[keyboard_index[1]][0]):
-            width, height = keyboard_win_rect[2] - keyboard_win_rect[0], keyboard_win_rect[3] - keyboard_win_rect[1]
-            key_x = keyboard_win_rect[0] + (keyboard_map[keyboard_index[1]][0][keyboard_index[0]] * (width - 214))
-            key_y = keyboard_win_rect[1] + (keyboard_map[keyboard_index[1]][1] * (height)) + 30
+    global mouse_pos, mode
+    try: keyboard_win_rect = win32gui.GetWindowRect(keyboard_win)
+    except Exception: mode = 'enabled'; return
+    width, height = keyboard_win_rect[2] - keyboard_win_rect[0], keyboard_win_rect[3] - keyboard_win_rect[1]
+    joysticks = get_joysticks()
+    
+    if mouse_pos[0] > keyboard_win_rect[0] and mouse_pos[0] < keyboard_win_rect[2] - 220 and \
+       mouse_pos[1] > keyboard_win_rect[1] + 80 and mouse_pos[1] < keyboard_win_rect[3]:
+        i_x, i_y = snap_keyboard(mouse_pos)
+        key_x, key_y = index_to_window((i_x, i_y))
+        mouse_pos = [key_x, key_y]
+        
+        mouse.move(key_x, key_y)
+        if button.A.get_new_press(): mouse.click()
+        
+        if button.LEFT_JOYSTICK_UP.get_repeated_new_press() or button.LEFT_JOYSTICK_DOWN.get_repeated_new_press() or \
+           button.LEFT_JOYSTICK_LEFT.get_repeated_new_press() or button.LEFT_JOYSTICK_RIGHT.get_repeated_new_press(): 
+            mouse_pos[0] += joysticks[0][0] * (width - 214) * (.5 * ((keyboard_map[i_y][0][i_x] - keyboard_map[i_y][0][i_x - 1]) if i_x > 0 else keyboard_map[i_y][0][i_x]) + 0.5 * ((keyboard_map[i_y][0][i_x + 1] - keyboard_map[i_y][0][i_x]) if i_x < len(keyboard_map[i_y][0] - 1) else keyboard_map[i_y][0][0]))
+            mouse_pos[1] -= joysticks[0][1] * 0.9 * keyboard_map[0][1] * height
+            mouse.move(*mouse_pos)
+        
+        if button.B.get_new_press():
+            mouse.move(
+                keyboard_win_rect[0] + (keyboard_map[0][0][14] * (width - 214)),
+                keyboard_win_rect[1] + (keyboard_map[0][1] * (height)) + 30
+            )
+            mouse.click()
+        
+        if button.Y.get_new_press():
+            mouse.move(
+                keyboard_win_rect[0] + (keyboard_map[4][0][4] * (width - 214)),
+                keyboard_win_rect[1] + (keyboard_map[4][1] * (height)) + 30
+            )
+            mouse.click()
+        
+        if button.X.get_new_press():
+            mouse.move(
+                keyboard_win_rect[0] + (keyboard_map[3][0][0] * (width - 214)),
+                keyboard_win_rect[1] + (keyboard_map[3][1] * (height)) + 30
+            )
+            mouse.click()
             
-            mouse.move(key_x, key_y)
-            if button.A.get_new_press(): mouse.click()
+        if button.UP.get_repeated_new_press() or button.RIGHT_JOYSTICK_UP.get_repeated_new_press():
+            mouse.move(
+                keyboard_win_rect[0] + (keyboard_map[3][0][11] * (width - 214)),
+                keyboard_win_rect[1] + (keyboard_map[3][1] * (height)) + 30
+            )
+            mouse.click()
             
-            if button.LEFT_JOYSTICK_UP.get_repeated_new_press(): 
-                keyboard_index[1] -= 1
-                mouse.move(key_x, keyboard_win_rect[1] + 60)
-                
-            if button.LEFT_JOYSTICK_DOWN.get_repeated_new_press(): 
-                keyboard_index[1] += 1
-                mouse.move(key_x, keyboard_win_rect[3] + 20)
-                
-            if button.LEFT_JOYSTICK_LEFT.get_repeated_new_press(): 
-                keyboard_index[0] -= 1
-                mouse.move(keyboard_win_rect[0] - 20, key_y)
-                
-            if button.LEFT_JOYSTICK_RIGHT.get_repeated_new_press(): 
-                keyboard_index[0] += 1
-                mouse.move(keyboard_win_rect[2] - 200, key_y)
+        if button.LEFT.get_repeated_new_press() or button.RIGHT_JOYSTICK_LEFT.get_repeated_new_press():
+            mouse.move(
+                keyboard_win_rect[0] + (keyboard_map[4][0][7] * (width - 214)),
+                keyboard_win_rect[1] + (keyboard_map[4][1] * (height)) + 30
+            )
+            mouse.click()
             
-            if button.B.get_new_press():
-                mouse.move(
-                    keyboard_win_rect[0] + (keyboard_map[0][0][14] * (width - 214)),
-                    keyboard_win_rect[1] + (keyboard_map[0][1] * (height)) + 30
-                )
-                mouse.click()
+        if button.DOWN.get_repeated_new_press() or button.RIGHT_JOYSTICK_DOWN.get_repeated_new_press():
+            mouse.move(
+                keyboard_win_rect[0] + (keyboard_map[4][0][8] * (width - 214)),
+                keyboard_win_rect[1] + (keyboard_map[4][1] * (height)) + 30
+            )
+            mouse.click()
             
-            if button.Y.get_new_press():
-                mouse.move(
-                    keyboard_win_rect[0] + (keyboard_map[4][0][4] * (width - 214)),
-                    keyboard_win_rect[1] + (keyboard_map[4][1] * (height)) + 30
-                )
-                mouse.click()
-            
-            if button.X.get_new_press():
-                mouse.move(
-                    keyboard_win_rect[0] + (keyboard_map[3][0][0] * (width - 214)),
-                    keyboard_win_rect[1] + (keyboard_map[3][1] * (height)) + 30
-                )
-                mouse.click()
-                
-            if button.UP.get_repeated_new_press() or button.RIGHT_JOYSTICK_UP.get_repeated_new_press():
-                mouse.move(
-                    keyboard_win_rect[0] + (keyboard_map[3][0][11] * (width - 214)),
-                    keyboard_win_rect[1] + (keyboard_map[3][1] * (height)) + 30
-                )
-                mouse.click()
-                
-            if button.LEFT.get_repeated_new_press() or button.RIGHT_JOYSTICK_LEFT.get_repeated_new_press():
-                mouse.move(
-                    keyboard_win_rect[0] + (keyboard_map[4][0][7] * (width - 214)),
-                    keyboard_win_rect[1] + (keyboard_map[4][1] * (height)) + 30
-                )
-                mouse.click()
-                
-            if button.DOWN.get_repeated_new_press() or button.RIGHT_JOYSTICK_DOWN.get_repeated_new_press():
-                mouse.move(
-                    keyboard_win_rect[0] + (keyboard_map[4][0][8] * (width - 214)),
-                    keyboard_win_rect[1] + (keyboard_map[4][1] * (height)) + 30
-                )
-                mouse.click()
-                
-            if button.RIGHT.get_repeated_new_press() or button.RIGHT_JOYSTICK_RIGHT.get_repeated_new_press():
+        if button.RIGHT.get_repeated_new_press() or button.RIGHT_JOYSTICK_RIGHT.get_repeated_new_press():
                 mouse.move(
                     keyboard_win_rect[0] + (keyboard_map[4][0][9] * (width - 214)),
                     keyboard_win_rect[1] + (keyboard_map[4][1] * (height)) + 30
                 )
                 mouse.click()
-            
+
     else:
-        joysticks = get_joysticks()
         mouse.move(joysticks[0][0] * sensitivity, joysticks[0][1] * -sensitivity, absolute=False, duration=.01)
         mouse.wheel(joysticks[1][1])
         if joysticks[1][0] != 0:
             keyboard.press("shift")
             mouse.wheel(-joysticks[1][0])
             keyboard.release("shift")
-        print(mouse.get_position())
-        print(keyboard_win_rect)
         if mouse.get_position()[0] > keyboard_win_rect[0] and \
            mouse.get_position()[0] < keyboard_win_rect[2] - 220 and \
            mouse.get_position()[1] > keyboard_win_rect[1] + 80 and \
            mouse.get_position()[1] < keyboard_win_rect[3]:
-            keyboard_index[1] = max(0, min(len(keyboard_map) - 1, keyboard_index[1]))
-            keyboard_index[0] = max(0, min(len(keyboard_map[keyboard_index[1]][0]) - 1, keyboard_index[0]))
+            mouse_pos = list(mouse.get_position())
                 
 def controller_loop():
     global last, dt, mode, keyboard_open_time, keyboard_win
